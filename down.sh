812731af73a9f22bf25abb17043473842baf2882
NatureZhang <71700470+naturezhanghn@users.noreply.github.com>
2025-02-01 11:47:26 +0800

    Update down.sh

--- down.sh
+++ down.sh
@@ -1,3 +1,4 @@
+#This can also be replaced with other datasets such as DotaV1, UC Merced Land Use dataset, etc.
 wget https://files.inria.fr/aerialimagelabeling/aerialimagelabeling.7z.001
 wget https://files.inria.fr/aerialimagelabeling/aerialimagelabeling.7z.002
 wget https://files.inria.fr/aerialimagelabeling/aerialimagelabeling.7z.003
